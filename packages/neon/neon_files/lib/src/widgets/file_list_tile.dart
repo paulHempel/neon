61184479878f7017eafdd97d39339c011fa937d8
jld3103 <jld3103yt@gmail.com>
2023-08-08 18:06:15 +0200

    feat(neon_files): Implement file syncing
    Signed-off-by: jld3103 <jld3103yt@gmail.com>

--- packages/neon/neon_files/lib/src/widgets/file_list_tile.dart
+++ packages/neon/neon_files/lib/src/widgets/file_list_tile.dart
@@ -127,7 +127,7 @@ class _FileIcon extends StatelessWidget {
             child: Icon(
               AdaptiveIcons.star,
               size: smallIconSize,
-              color: Colors.yellow,
+              color: NcColors.starredColor,
             ),
           ),
         ],
